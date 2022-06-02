85967769bbf9eb17f9c886a550441642a5d59d67
a.dehut <a.dehut@nextkidney.com>
2022-06-02 09:52:29 +0200

    remove toitoinou unused apps

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -1,7 +1,7 @@
 #include "displayapp/DisplayApp.h"
 #include <libraries/log/nrf_log.h>
 #include "displayapp/screens/HeartRate.h"
-#include "displayapp/screens/Motion.h"
+//#include "displayapp/screens/Motion.h"
 #include "displayapp/screens/Timer.h"
 #include "displayapp/screens/Alarm.h"
 #include "components/battery/BatteryController.h"
